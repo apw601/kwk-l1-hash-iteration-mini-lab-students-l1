0c34155945cb2444ec0b924cbc905dadd2a590fa
apw601 <apw601@gmail.com>
2018-06-06 22:09:47 +0000

    automatically backed up by learn

--- hashes.rb
+++ hashes.rb
@@ -1,27 +1,26 @@
-class_celeb_crushes = {
-  "Natalia" => "Timothee Chalamet",
-  "Maya" => "Michael B. Jordan",
-  "Amira" => "Cole Sprouse",
-}
+# class_celeb_crushes = {
+#   "Natalia" => "Timothee Chalamet",
+#   "Maya" => "Michael B. Jordan",
+#   "Amira" => "Cole Sprouse",
+# }
 
-class_celeb_crushes.each do |names, crushes|
-  puts "#{names}'s crush is #{crushes}"
-end
-
-
-
-
-# puts class_celeb_crushes
-
-# character_names = ["Monica", "Joey", "Phoebe"]
-# characteristics = ["Neat Freak", "Jokester", "Free Spirit"]
-
-# friends_characters = {}
-
-# index = 0
-# character_names.each do |name|
-#   friends_characters[name] = characteristics[index]
-#   index += 1
+# class_celeb_crushes.each do |names, crushes|
+#   puts "#{names}'s crush is #{crushes}"
 # end
 
-# puts friends_characters
+
+
+# # puts class_celeb_crushes
+
+# # character_names = ["Monica", "Joey", "Phoebe"]
+# # characteristics = ["Neat Freak", "Jokester", "Free Spirit"]
+
+# # friends_characters = {}
+
+# # index = 0
+# # character_names.each do |name|
+# #   friends_characters[name] = characteristics[index]
+# #   index += 1
+# # end
+
+# # puts friends_characters
